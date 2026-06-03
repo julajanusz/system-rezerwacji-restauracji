9be3e6fb1b1b59485481c850d4b33b016f4feacc
Julia Janusz <julia.zofia.janusz@gmail.com>
2026-06-03 22:33:46 +0200

    Zainstalowanie pootrzebnych Pakietów NuGet,  konfiguracja bazy danych, connection string.

---
+++ System_rezerwacji_stolikow/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System_rezerwacji_stolikow.Models;
+
+namespace System_rezerwacji_stolikow.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
